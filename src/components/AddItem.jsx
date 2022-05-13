5b76a7e2ef9774f2e0d0cac17de79caadc0a85ad
Benatovich <sambenatovich@gmail.com>
2022-05-13 13:45:28 -0400

    List.jsx set up, started working on AddItem.jsx

---
+++ src/components/AddItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function AddItem() {
+    // const [newItem, setNewItem] = useState('');
+    // const { items, handleAddItem, handleUpdateItem, handleDeleteItem } = useList();
+
+
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     handleAddItem(newItem);
+    //     setNewItem('');
+    // };
+  return (
+    <div>AddItem</div>
+  )
+}
--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,7 +1,26 @@
-import { useState } from 'react'
+import Item from '../components/Item'
+import AddItem from '../components/AddItem'
+import { useList } from '../context/ListProvider'
 
 export default function List() {
+  const { items, handleUpdateItem, handleDeleteItem } = useList();
+
+
   return (
-    <div>List</div>
-  )
+    <div>
+      <h1>Shopping List:</h1>
+      <AddItem />
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <Item
+              item={item}
+              onUpdate={handleUpdateItem}
+              onDelete={handleDeleteItem}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
