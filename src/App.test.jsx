5a444ae72b7260d24be9bda9fde3c1a77f6a7541
Benatovich <sambenatovich@gmail.com>
2022-05-13 17:48:55 -0400

    display, create, and update tests all pass

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -14,3 +14,48 @@ it('displays a list of items', () => {
 
     expect(itemArray).toEqual([bread, eggs, milk])
 })
+
+it('creates a new item', () => {
+    const inputBox = screen.getByRole('textbox');
+    userEvent.type(inputBox, 'new item');
+    
+    const button = screen.getByRole('button', {  name: /add item/i});
+    userEvent.click(button);
+
+    const newItem = screen.getByText(/new item/i);
+    
+    expect(newItem).toBeInTheDocument();
+})
+
+it('updates an existing item', () => {
+    // const milk = screen.getByText(/gallon of milk/i);
+    const updateButton = screen.getByRole('button', {  name: /edit gallon of milk/i});
+    userEvent.click(updateButton);
+
+    // const list = screen.getByRole('list');
+    // const inputBox = within(list).getByRole('textbox');
+    const inputBox = screen.getByDisplayValue(/gallon of milk/i)
+    userEvent.type(inputBox, 'beer');
+
+    const saveButton = screen.getByRole('button', {  name: /save changes/i});
+    userEvent.click(saveButton);
+
+    const beer = screen.getByText(/beer/i);
+    expect(beer).toBeInTheDocument();
+})
+
+// it('should increment the counter when increment pressed', () => {
+//     const counter = screen.getByRole('heading');
+//     expect(counter).toHaveTextContent('0');
+//     expect(counter).toHaveStyle(`color: ${yellow}`);
+  
+//     const incrementButton = screen.getByRole('button', { name: 'increment' });
+//     userEvent.click(incrementButton);
+//     expect(counter).toHaveTextContent('1');
+//     expect(counter).toHaveStyle(`color: ${green}`);
+  
+//     userEvent.click(incrementButton);
+//     expect(counter).toHaveTextContent('2');
+//     expect(counter).toHaveStyle(`color: ${green}`);
+//   });
+  
