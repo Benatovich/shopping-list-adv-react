a5f1ce08802a6328fa7e9a80698e00046527dba9
Benatovich <sambenatovich@gmail.com>
2022-05-13 18:42:13 -0400

    looks good enough to me

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import Header from './components/Header'
 import { ListProvider } from './context/ListProvider'
 import List from './views/List'
+import './App.css'
 
 export default function App() {
   return (
-    <>
+    <div className='App' style={{}}>
       <ListProvider>
         <Header />
-        <List />
+        <div className='App-main' style={{textAlign: 'center', backgroundColor: '#232f46',  display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'top', fontSize: 'calc(10px + 2vmin)', color: 'beige', margin: 0, borderRadius: 30}}>
+          <List />
+        </div>
       </ListProvider>
-    </>
+    </div>
   );
 }
+
+{/* <div className='App' style={{backgroundColor: '#232f46', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', color: 'beige', borderRadius: 30}}> */}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,9 @@ export default function Header() {
   const { items } = useList();
 
   return (
-    <div>Number of Shopping List Items Remaining: {items.length}</div>
+    <div style={{backgroundColor: 'orange', borderRadius: 30, padding: 0, margin: 5, minHeight: '10vh', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'right', fontSize: 'calc(10px + 1.5vmin)'}}>
+      <p style={{backgroundColor: '#232f46', color: 'beige', borderRadius: 10, padding: 5}}>Total items: </p>
+      <p style={{backgroundColor: 'beige', color: '#232f46', borderRadius: 10, padding: 5, paddingLeft: 10, paddingRight: 10, marginLeft: 10, marginRight: 10}}>{items.length}</p>
+    </div>
   )
 }
